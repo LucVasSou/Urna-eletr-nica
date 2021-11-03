a52aa1d3c22565f69d7a3e623d9d28dd76817c21
LucVasSou <lucasbmbr@hotmail.com>
2021-11-03 15:47:23 -0300

    Versão 1

---
+++ etapas.js
@@ -0,0 +1,19 @@
+let etapas = [
+  {
+    titulo: 'PROGRAMADOR',
+    numeros: 11,
+    candidatos: [
+      {
+        numero: '24988369117',
+        nome: 'Lucas Vasconcelos de Souza',
+        partido: 'NovoDEV',
+        fotos: [
+          {
+            url: 'foto Lucas.jpg',
+            legenda: 'Programador'
+          }
+        ]
+      }
+    ]
+  }
+]
